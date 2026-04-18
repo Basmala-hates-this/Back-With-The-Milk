ce6c14c9a7e809f745c35649248307bf318d1f37
Basmala-hates-this <laouaribasmala75@gmail.com>
2026-04-18 15:30:09 +0100

    adding 2 fileds to display according to the tp... i'll remove them after

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const button = document.getElementById('fetch-btn');
 const display = document.getElementById('message-container');
+const metaDisplay = document.getElementById('meta-container');
 
 button.addEventListener('click', async () => {
     button.disabled = true;
     display.innerText = "Scanning the Dad-abase...";
+    metaDisplay.innerText = "";
+
     
     
     try {
-        // Technical Note: We MUST send an 'Accept' header for this API
+        // Technical Note: We MUST send an 'Accept' header for this API....wished i stayed with the simple unhinged api
         const response = await fetch('https://icanhazdadjoke.com/', {
             headers: {
                 'Accept': 'application/json'
@@ -16,8 +19,12 @@ button.addEventListener('click', async () => {
         
         const data = await response.json();
         
-        // The joke is stored in the 'joke' field
+        // The joke is stored in the 'joke' field....joke on u my life is a joke...sorry
         display.innerText = data.joke;
+        metaDisplay.innerHTML = `
+            <span>the id of the joke(second get): ${data.id} </span> <br>
+            <span>the status of http replay(third result): ${data.status}</span>
+        `;
        
 
     } catch (error) {
